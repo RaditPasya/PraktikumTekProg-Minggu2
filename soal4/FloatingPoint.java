8519e430719c5b121298901f8be1acca23958fb5
RaditPasya <radityaphr@gmail.com>
2022-02-12 14:58:28 +0700

    Soal 4 - Operators 1

---
+++ soal4/FloatingPoint.java
@@ -0,0 +1,10 @@
+package soal4;
+
+class FloatingPoint{
+    public static void main(String[] args) {
+    double x = 92.98;
+    int nx = (int) Math.round(x);
+    System.out.println(nx);
+    }
+   }
+   
